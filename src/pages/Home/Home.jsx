08d01062d27a137787bc8a0c6db6c29b0605e568
filiphubler <filip.hubler@student.spsmb.cz>
2024-05-24 21:53:37 +0200

    formated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
-import Banner from "../../components/Ui/Banner"
-import Footer from "../../components/Ui/Footer"
+import Banner from "../../components/Ui/Banner";
+import Footer from "../../components/Ui/Footer";
 
 export default function Home() {
   return (
     <>
-    <Banner></Banner>
-    <Footer></Footer>
+      <Banner></Banner>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, et?
+        Adipisci illo suscipit quaerat ex amet consequuntur eius? Dolor
+        perferendis autem quod debitis quae perspiciatis blanditiis, in soluta
+        veniam at! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Dolore voluptatibus saepe totam soluta? Dolorum totam minima, quas odit
+        aspernatur aperiam voluptatibus aliquam quibusdam hic architecto illo
+        repellendus deleniti amet quisquam?
+      </p>
+      <Footer></Footer>
     </>
-  )
+  );
 }
