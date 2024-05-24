d19401ce369c60ad2a30e348acf59da87b054560
filiphubler <filip.hubler@student.spsmb.cz>
2024-05-24 22:31:46 +0200

    title name

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import Banner from "../../components/Ui/Banner";
 import Footer from "../../components/Ui/Footer";
+import React, { useEffect } from 'react';
 
 export default function Home() {
+
+  useEffect(() => {
+    document.title = "Obec Dalovice";
+  }, []);
+    
+  
   return (
     <>
       <Banner></Banner>
@@ -17,4 +24,6 @@ export default function Home() {
       <Footer></Footer>
     </>
   );
+
+
 }
