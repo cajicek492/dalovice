6003ebd9d9834065a0a23018fa9365cde463bb02
filiphubler <filip.hubler@student.spsmb.cz>
2024-05-26 11:42:52 +0200

    Cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,21 @@
 import Banner from "../../components/Ui/Banner";
 import Footer from "../../components/Ui/Footer";
+
 import React, { useEffect } from 'react';
+
 
 export default function Home() {
 
   useEffect(() => {
     document.title = "Obec Dalovice";
   }, []);
-    
-  
   return (
     <>
       <Banner></Banner>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, et?
-        Adipisci illo suscipit quaerat ex amet consequuntur eius? Dolor
-        perferendis autem quod debitis quae perspiciatis blanditiis, in soluta
-        veniam at! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Dolore voluptatibus saepe totam soluta? Dolorum totam minima, quas odit
-        aspernatur aperiam voluptatibus aliquam quibusdam hic architecto illo
-        repellendus deleniti amet quisquam?
-      </p>
+     
       <Footer></Footer>
     </>
   );
 
-
+ 
 }
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,10 +1,70 @@
 import Banner from "../../components/Ui/Banner"
 import Footer from "../../components/Ui/Footer"
+import Card from "../../components/Ui/Card";
 
 export default function News() {
   return (
     <>
     <Banner></Banner>
+    <br></br>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <div className="flex justify-center">
+            <Card
+              image="https://via.placeholder.com/150"
+              title="Sample Card"
+              description="This is a sample description for the card component."
+              buttonText="Click Me"
+            
+            />
+          </div>
+          <div className="flex justify-center">
+            <Card
+              image="https://via.placeholder.com/150"
+              title="Sample Card"
+              description="This is a sample description for the card component."
+              buttonText="Click Me"
+             
+            />
+          </div>
+          <div className="flex justify-center">
+            <Card
+              image="https://via.placeholder.com/150"
+              title="Sample Card"
+              description="This is a sample description for the card component."
+              buttonText="Click Me"
+              
+            />
+          </div>
+          <div className="flex justify-center">
+            <Card
+              image="https://via.placeholder.com/150"
+              title="Sample Card"
+              description="This is a sample description for the card component."
+              buttonText="Click Me"
+             
+            />
+            
+          </div>
+          <div className="flex justify-center">
+            <Card
+              image="https://via.placeholder.com/150"
+              title="Sample Card"
+              description="This is a sample description for the card component."
+              buttonText="Click Me"
+             
+            />
+          </div>
+          <div className="flex justify-center">
+            <Card
+              image="https://via.placeholder.com/150"
+              title="Sample Card"
+              description="This is a sample description for the card component."
+              buttonText="Click Me"
+             
+            />
+          </div>
+        </section>
+        <br></br>
     <Footer></Footer>
     </>
   )
