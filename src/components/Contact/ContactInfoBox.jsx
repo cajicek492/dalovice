0f4697998d8d9ee051ec2cffe72e8312c70a84bd
filiphubler <filip.hubler@student.spsmb.cz>
2024-05-31 18:21:38 +0200

    Google maps

--- src/components/Contact/ContactInfoBox.jsx
+++ src/components/Contact/ContactInfoBox.jsx
@@ -12,7 +12,7 @@ const ContactInfoBox = ({
   dataBoxId,
   ico,
   imageSrc,
-  imageAlt
+  imageAlt,
 }) => {
   return (
     <div className="bg-gray-900 text-gray-200 p-6 border border-gray-700 rounded-lg shadow-lg mt-8 mb-12 mx-4 md:mx-auto md:max-w-4xl">
@@ -36,6 +36,17 @@ const ContactInfoBox = ({
         alt={imageAlt}
         className="w-full h-auto rounded-lg mb-4"
       />
+      <div className="relative" style={{ paddingBottom: "56.25%" }}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4549.606367543052!2d14.882314220485249!3d50.42243282071147!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470955d125796835%3A0x21bf8eae7fba32ab!2zRGFsb3ZpY2UgLSBPYmVjbsOtIMO6xZlhZA!5e0!3m2!1scs!2scz!4v1717171134394!5m2!1scs!2scz"
+          title="Google Map"
+          frameBorder="0"
+          className="absolute inset-0 w-full h-full rounded-lg"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </div>
   );
 };
--- src/components/School/SchoolInfoBox.jsx
+++ src/components/School/SchoolInfoBox.jsx
@@ -13,7 +13,7 @@ const SchoolInfoBox = ({
   phone,
   email,
   imageSrc,
-  imageAlt
+  imageAlt,
 }) => {
   return (
     <div className="bg-gray-900 text-gray-200 p-6 border border-gray-700 rounded-lg shadow-lg mt-8 mb-12 mx-4 md:mx-auto md:max-w-4xl">
@@ -38,6 +38,17 @@ const SchoolInfoBox = ({
         alt={imageAlt}
         className="w-full h-auto rounded-lg mb-4"
       />
+      <div className="relative" style={{ paddingBottom: "56.25%" }}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4549.230299238749!2d14.87605430989602!3d50.42634758250073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470955d65ad465e9%3A0x5f585c451cffd70b!2zWsOha2xhZG7DrSDFoWtvbGE!5e0!3m2!1scs!2scz!4v1717172259514!5m2!1scs!2scz"
+          title="Google Map"
+          frameBorder="0"
+          className="absolute inset-0 w-full h-full rounded-lg"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </div>
   );
 };
